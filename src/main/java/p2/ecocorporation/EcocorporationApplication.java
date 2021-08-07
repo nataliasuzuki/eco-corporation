721be485ddf159d2d8d6280682bbe65f2dcb1b9a
nataliasuzuki <61856025+nataliasuzuki@users.noreply.github.com>
2021-08-07 08:55:14 -0300

    Add files via upload
    Inclusão do projeto para o github

---
+++ src/main/java/p2/ecocorporation/EcocorporationApplication.java
@@ -0,0 +1,13 @@
+package p2.ecocorporation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcocorporationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcocorporationApplication.class, args);
+	}
+
+}
