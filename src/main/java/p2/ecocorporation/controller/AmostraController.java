dafa0b6977fbe1daf9e5909e7cf8e557da846bb1
Natalia Lie Suzuki <liesuzukin@gmail.com>
2021-08-07 12:12:09 -0300

    Consulta de desempenho por funcionario, por regiao ou por trimestre

--- src/main/java/p2/ecocorporation/controller/AmostraController.java
+++ src/main/java/p2/ecocorporation/controller/AmostraController.java
@@ -1,5 +1,9 @@
 package p2.ecocorporation.controller;
 
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import p2.ecocorporation.model.Amostra;
@@ -42,6 +47,72 @@ public class AmostraController {
 	  @PathVariable("codigo") Integer codigo) {
 		repository.save(amostra);
 	    return ResponseEntity.ok("Amostragem atualizado com sucesso!");
+	}
+	
+	@GetMapping(path = "/amostra/desempenho")
+	public String consultarDesempenhoF(@RequestParam Map<String,String> params) {
+		
+		String tipoConsulta = "";
+		String busca = "";
+		
+		for (Entry<String, String> entry : params.entrySet()) 
+		{
+		    System.out.println(entry.getKey() + "/" + entry.getValue());
+		    tipoConsulta = entry.getKey();
+		    busca = entry.getValue();
+		}
+		
+		Double cota = 0.0;
+		Double vendas = 0.0;
+		Double desempenho = 0.0;
+		String trimestre = "";
+		String regiao = "";
+		String funcionario;
+		String result = "";
+		String resultTemp = "";
+		List<Amostra> amostra = null;
+		
+		if(tipoConsulta.equalsIgnoreCase("funcionario"))
+		{
+			result = "Funcionario: " + busca;
+			amostra = repository.findByFuncionario(busca);
+		}
+		else if(tipoConsulta.equalsIgnoreCase("regiao"))
+		{
+			result = "Regiao: " + busca;
+			amostra = repository.findByRegiao(busca);
+		}
+		else
+		{
+			result = "Trimestre: " + busca;
+			amostra = repository.findByTrimestre(busca);
+		}
+
+		for(Amostra registro : amostra)
+		{
+			cota = registro.getCota();
+			vendas = registro.getVendas();
+			trimestre = registro.getTrimestre();
+			funcionario = registro.getFuncionario();
+			regiao = registro.getRegiao();
+			desempenho = desempenho + ((vendas*100) / cota);
+			
+			if(tipoConsulta.equalsIgnoreCase("funcionario"))
+			{
+				resultTemp = resultTemp + "\n\nTrimestre: " + trimestre + "\nCota: " + cota + "\nVendas: " + vendas + "\nRegiao: " + regiao;
+			}
+			else if(tipoConsulta.equalsIgnoreCase("regiao"))
+			{
+				resultTemp = resultTemp + "\n\nTrimestre: " + "" + "\nCota: " + cota + "\nVendas: " + vendas + "\nFucionario: " + funcionario;
+			}
+			else
+			{
+				resultTemp = resultTemp + "\n\nCota: " + cota + "\nVendas: " + vendas + "\nFuncionario: " + funcionario + "\nRegiao: "+ regiao;
+			}
+		}
+		result = result + "\nDesempenho: " + new DecimalFormat("#.##").format(desempenho) + "%" + 
+				"\n\n-----HISTORICO-----" + resultTemp + "\n-------------------";
+		return result;
 	}
 
 }
--- src/main/java/p2/ecocorporation/repository/AmostraRepository.java
+++ src/main/java/p2/ecocorporation/repository/AmostraRepository.java
@@ -1,8 +1,23 @@
 package p2.ecocorporation.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import p2.ecocorporation.model.Amostra;
 
 public interface AmostraRepository extends CrudRepository<Amostra, Integer> {
+
+	@Query
+	("select a from amostra a where a.funcionario = :funcionario")
+	List<Amostra> findByFuncionario(String funcionario);
+
+	@Query
+	("select a from amostra a where a.regiao = :regiao")
+	List<Amostra> findByRegiao(String regiao);
+
+	@Query
+	("select a from amostra a where a.trimestre = :trimestre")
+	List<Amostra> findByTrimestre(String trimestre);
 
 }
