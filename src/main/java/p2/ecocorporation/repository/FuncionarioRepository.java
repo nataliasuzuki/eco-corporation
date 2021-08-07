904a66458c1fbd8ddb512bbf5c6290d7915845fa
Natalia Lie Suzuki <liesuzukin@gmail.com>
2021-08-07 20:05:13 -0300

    Remomido Funcionario, não tem utilização

--- src/main/java/p2/ecocorporation/repository/FuncionarioRepository.java
+++
@@ -1,8 +0,0 @@
-package p2.ecocorporation.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import p2.ecocorporation.model.Funcionario;
-
-public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
-
-}
