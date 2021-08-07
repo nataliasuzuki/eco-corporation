ee03832adfcda923027236c4d16bd7ed0dce4d98
nataliasuzuki <61856025+nataliasuzuki@users.noreply.github.com>
2021-08-07 08:52:53 -0300

    Add files via upload
    Inclusão do projeto para o github

---
+++ ecocorporation/src/test/java/p2/ecocorporation/EcocorporationApplicationTests.java
@@ -0,0 +1,13 @@
+package p2.ecocorporation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcocorporationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
